71bbf52636229690ff701a482d79bce969906f25
Toni Buenter <toni@1234.ch>
2024-04-17 12:11:26 +0200

    version 0.0.22

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import { Config, init, isalnum, namedParameters2QuestionMarks } from './remotequery-mysql';
+import * as camelCase from 'camelCase';
 
 import type { InitProps } from './types';
 import MySqlDriver from './MySqlDriver';
 
-export { InitProps, MySqlDriver };
+export { InitProps, MySqlDriver, camelCase };
